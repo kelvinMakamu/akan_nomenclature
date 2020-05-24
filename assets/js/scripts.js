a6e006b0b161d532cf16eb2ed21ba8c18c353f9b
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-05-24 22:14:23 +0300

    Modifies scripts.js file,adds business logic to validate month entered

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,16 @@
 /*************
 
+	CONSTANTS
+
+*******************/
+const thirtyDaysMonth=[4,6,9,11];
+const thirtyOneDaysMonth=[1,3,5,7,8,10,12];
+const femaleAkanNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+const maleAkanNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+
+
+/*************
+
 	VALIDATIONS
 
 *******************/
@@ -19,7 +30,7 @@ let validateDate = (birthDate) => {
 		break;
 
 		case 1001:
-		errorDate.innerHTML="Invalid. Enter DIGITS ONLY";
+		errorDate.innerHTML="Invalid Input. Enter DIGITS ONLY";
 		break;
 	}
 }
@@ -49,6 +60,15 @@ let checkNumberLimit = (number,checkType) => {
 		break;
 
 		case 'month':
+		if(numberLength > 0 && numberLength <=2){
+			if(number > 0 && number <=12){
+				checkStatus=1000;
+			}else{
+				checkStatus=1003;
+			}
+		}else{
+			checkStatus=1002;
+		}
 		break;
 
 		case 'year':
@@ -75,4 +95,81 @@ let clearFormErrorMessages= (errorType) => {
 		document.getElementById("yearHelp").innerHTML="";
 		break;
 	}
+}
+
+let validateMonth = (birthMonth) => {
+	let errorMonth=document.getElementById("monthHelp");
+	switch(validateNumber(birthMonth)){
+		case 1000:
+		clearFormErrorMessages('month');
+		checkStatus=checkNumberLimit(birthMonth,'month');
+		if(checkStatus == 1000){
+			dateMonthStatus=validateDateAndMonth(birthMonth);
+			switch(dateMonthStatus){
+				case 1000:
+				clearFormErrorMessages('month');
+				break;	
+
+				case 1004:
+				errorMonth.innerHTML="Dates must be at MOST 2 DIGITS ONLY.";
+				break;
+
+				case 1005:
+				errorMonth.innerHTML="The Month has at MOST 30 DAYS.";
+				break;
+
+				case 1006:
+				errorMonth.innerHTML="The Month has at MOST 31 DAYS.";
+				break;
+
+				case 1007:
+				errorMonth.innerHTML="The Month has at MOST 29 DAYS.";
+				break;
+			}
+		}else if(checkStatus == 1002){
+			errorMonth.innerHTML="Months must be at MOST 2 DIGITS ONLY.";
+		}else if(checkStatus == 1003){
+			errorMonth.innerHTML="Months must not be GREATER than 12.";
+		}
+		break;
+
+		case 1001:
+		errorMonth.innerHTML="Invalid Input. Enter DIGITS ONLY";
+		break;
+	}
+}
+
+let validateDateAndMonth = (birthMonth) => {
+	let birthDate=document.getElementById("date").value;
+	checkStatus=checkNumberLimit(birthDate,'date');
+	if(checkStatus == 1000){
+		switch(parseInt(birthMonth)){
+			case 2:
+			if(parseInt(birthDate) > 29){
+				dateMonthStatus=1007;//does not have more than 29 days.
+			}else{
+				dateMonthStatus=1000;
+			}
+			break;
+
+			default:
+			if(thirtyDaysMonth.includes(parseInt(birthMonth))){
+				if(parseInt(birthDate) > 30){
+					dateMonthStatus=1005;//does not have more than 30 days.
+				}else{
+					dateMonthStatus=1000;
+				}
+			}else if(thirtyOneDaysMonth.includes(parseInt(birthMonth))){
+				if(parseInt(birthDate) > 31){
+					dateMonthStatus=1006;//does not have more than 31 days.
+				}else{
+					dateMonthStatus=1000;
+				}
+			}
+			break;
+		}
+	}else{
+		dateMonthStatus=1004;
+	}
+	return dateMonthStatus;
 }
